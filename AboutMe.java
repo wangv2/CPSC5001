00f1a1b653357b884427b05ad83de6ce7111e749
wangv2 <veeveew@gmail.com>
2017-06-24 14:20:22 -0700

    Create AboutMe.java
    Lab 1

---
+++ AboutMe.java
@@ -0,0 +1,16 @@
+// Vilia Wang
+// AboutMe.java
+// Purpose: This is a sample program that will print
+//          "Name: Vilia
+//          Favorite Color: Green
+//          Place of Birth: Bellevue, WA" to screen.
+public class AboutMe
+{
+   public static void main(String[] args)
+     {
+        System.out.println("Name: Vilia");
+        System.out.println("Favorite Color: Green");
+        System.out.println("Place of Birth: Bellevue, WA");
+     }
+  }
+      
